d65994d6d0b4b9ecc7932d833b90819ebc3fca1c
ry4nnnn <hjzhu@hillstonenet.com>
2024-08-05 08:59:20 +0800

    fix bug

--- collectors/decorators.py
+++ collectors/decorators.py
@@ -3,21 +3,24 @@ import requests
 from functools import wraps
 import time
 
+from notifications.notifier import send_realtime_notifications
+
 
 def retry(max_retries=3, delay=2):
     def decorator(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(self, *args, **kwargs):
             retries = 0
             while retries < max_retries:
                 try:
-                    return func(*args, **kwargs)
+                    return func(self, *args, **kwargs)
                 except requests.RequestException as e:
                     print(f"Request failed: {e}. Retrying {retries + 1}/{max_retries}...")
                     retries += 1
                     time.sleep(delay)
-            print("Max retries reached. Exiting.")
-            raise SystemExit("Max retries reached. Exiting.")
+            msg = f"fail to fetch {self.source_name} data due to network error"
+            send_realtime_notifications(msg)
+            return None
 
         return wrapper
 
